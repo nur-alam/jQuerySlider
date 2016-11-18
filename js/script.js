bb4649c3b85a1469ff51cbeca8b03054d8b0cba0
nuralam <nuralam862@gmail.com>
2016-11-18 16:44:07 +0600

    complete to making a slider

--- js/script.js
+++ js/script.js
@@ -4,75 +4,91 @@
 		var _gallery = $('#slider');
 		_gallery.find('ul').attr('class','slides');
 		_gallery.find('ul>li').attr('class','slide');
-	    _gallery.find('ul>li:first').addClass('active');
 		var _slides = _gallery.find('.slides');
 		var _slide = _gallery.find('.slide');
 		
 	
 		_gallery.find('li:first').clone(true).appendTo(_gallery.find('ul'));
-		//_gallery.find('li:last').clone(true).prependTo(_gallery.find('ul'));
+		_gallery.find('ul>li:first').addClass('active');
 	
 		totalSlides = _gallery.find('li').length;
-		mainDivWidth = _gallery.outerWidth();
-	    eachSlideWidth = _gallery.find('li').outerWidth();
+		mainDivWidth = _gallery.outerWidth(true);
+	    eachSlideWidth = _gallery.find('li').outerWidth(true);
 		totalWidth = totalSlides*eachSlideWidth;
+	
+		console.log(mainDivWidth);
+		console.log(eachSlideWidth);
+		console.log(totalWidth);
 	
 		_gallery.find('ul').css('width',totalWidth);
 	    _gallery.find('ul>li').css('width',mainDivWidth);
 	
-		var _current_slide = 1;
-		
 	
-	
-/*		$('.prev_area').click(function(event){
+		/*previous sliding function*/
+		$('.prev_area').click(function(event){
 			event.preventDefault();
-			var first = _slides.find('li').first();
-			var last  = _slides.find('li').last();
-			//_slides.prepend(last);
-			//_slides.animate({'margin-left':'+='+mainDivWidth},350,'linear');
-			last.insertBefore(first);
+			
+			/*getting the index of active li element*/
+			index = _gallery.find('.active').index();
+			
+			active = _gallery.find('.active');
+			activeNext = active.prev();
+			console.log(active);
+			console.log(activeNext);
+			
+			/*if active li element is first li then go to last slide*/
+			if(index == 0){
+				activeNext = _gallery.find('ul>li').eq(totalSlides-2);
+				activeNext.addClass('active');
+				firstActive = _gallery.find('.active').eq(0);
+				firstActive.removeClass('active');
+				_slides.css('margin-left',-(totalWidth-eachSlideWidth));
+				_slides.animate({'margin-left':'+='+mainDivWidth},750,'swing');
+			}else{
+				activeNext.addClass('active');
+				active.removeClass('active');
+				_slides.animate({'margin-left':'+='+mainDivWidth},750,'swing');	
+			}
+			
 		});	
-*/	
-/*		$('.next_area').click(function(event){
-			event.preventDefault();
-			var first = _slides.find('li').first();
-			var last  = _slides.find('li').last();
-			console.log(first);
-			_slides.animate({'margin-left':'-='+mainDivWidth},1000,'linear',function(){
-				//first.insertAfter(last);	
-				console.log(first);
-			});
-			
-		});	*/
 	
 	
-/*		$('.prev_area').click(function(event){
-			event.preventDefault();
-			
-			_slides.animate({'margin-left':'+='+mainDivWidth},350,'linear',function(){
-				_current_slide++;console.log(_current_slide);
-				
-				if(_current_slide===totalSlides){
-					_current_slide = 1;
+	
+		/* next sliding function*/
+		function nextSlideing(){
+
+			/* getting the index of active li element*/
+			index = _gallery.find('.active').index();
+
+			active = _gallery.find('.active');
+			activeNext = active.next();
+			activeNext.addClass('active');
+			firstActive = _gallery.find('.active').eq(0);
+			firstActive.removeClass('active');
+
+			_slides.animate({'margin-left':'-='+mainDivWidth},750,'swing',function(){
+				/*if active index is last li element then add active class to first li element*/
+				if(index===(totalSlides-2)){
+					_gallery.find('ul>li').removeClass('active');
+					_gallery.find('ul>li:first').addClass('active');
 					_slides.css('margin-left',0);
 				}
 			});
-		});	
+
+		}
 	
-		$('.next_area').click(function(event){
+	
+		/* next cliking event*/
+		$(document).on('click','.next_area',function(event){
 			event.preventDefault();
-			
-			_slides.animate({'margin-left':'-='+mainDivWidth},350,'linear',function(){
-				_current_slide++;console.log(_current_slide);
-				
-				if(_current_slide===totalSlides){
-					_current_slide = 1;
-					_slides.css('margin-left',0);
-				}
-			});
-		});*/
+			nextSlideing();
+		});
 	
-		setInterval(function(){
+	
+		/*making auto sliding*/
+		/*setInterval(nextSlideing,6000);*/
+	
+/*		setInterval(function(){
 			_slides.animate({'margin-left' : '-='+mainDivWidth},'linear',function(){
 				_current_slide++;
 				if(_current_slide === totalSlides){
@@ -80,13 +96,12 @@
 					_slides.css('margin-left',0);
 				}
 			});
-		},2000); 
+		},2000); */
 	
 		//console.log(totalSlides);
 	
 	
 }(jQuery))
-
 
 
 
